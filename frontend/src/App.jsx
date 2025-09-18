b4c27a0e3d3e086755e53484095386714b886e38
Wiredan app <dansidran@gmail.com>
2025-09-18 14:03:03 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import KYC from "./pages/KYC";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/kyc" element={<KYC />} />
-        <Route path="/" element={<h1>Welcome to AgriNetwork 🚜</h1>} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+<Routes>
+  <Route path="/signup" element={<Signup />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/kyc" element={<KYC />} />
+  <Route path="/dashboard" element={<Dashboard />} />
+  <Route path="/" element={<h1>Welcome to AgriNetwork 🚜</h1>} />
+</Routes>
