485553da152c296de2c6bf200cf1ee42c95a2176
Wiredan app <dansidran@gmail.com>
2025-09-18 14:06:13 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import Dashboard from "./pages/Dashboard";
+import Marketplace from "./pages/Marketplace";
 
 <Routes>
   <Route path="/signup" element={<Signup />} />
   <Route path="/login" element={<Login />} />
   <Route path="/kyc" element={<KYC />} />
   <Route path="/dashboard" element={<Dashboard />} />
+  <Route path="/marketplace" element={<Marketplace />} />
   <Route path="/" element={<h1>Welcome to AgriNetwork 🚜</h1>} />
 </Routes>
